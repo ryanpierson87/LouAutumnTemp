a65833ad781876c042ad9b0704ec2ca533fdc4cb
Ryan Pierson <ryanpierson87@protonmail.com>
2018-11-12 19:17:29 -0500

    changed autumn seaborn colors

--- LouisvilleTemperature.py
+++ LouisvilleTemperature.py
@@ -35,10 +35,10 @@ def main():
     #Box/Violin plots of Autumn
     autumn = weather[weather['MONTH'].isin([9, 10, 11])]
     plt.subplots(figsize=(20,10))
-    sns.boxplot(x=autumn['YEAR'], y=autumn['Temp'], color="#e64d00").set_title("Box Autumnal Comparison of Temperatures")
+    sns.boxplot(x=autumn['YEAR'], y=autumn['Temp'], color="#ff7733").set_title("Box Autumnal Comparison of Temperatures")
     plt.grid(axis='y', color=x)
     plt.subplots(figsize=(20,10))
-    sns.violinplot(x=autumn['YEAR'], y=autumn['Temp'], color="#e64d00").set_title("Violin Autumnal Comparison of Temperatures")
+    sns.violinplot(x=autumn['YEAR'], y=autumn['Temp'], color="#ff7733").set_title("Violin Autumnal Comparison of Temperatures")
     plt.grid(axis='y', color=x)
     
     
